3a790303dad30680eca0635ed777d8af4e5d72bf
cristobal <tobalin01@gmail.com>
2018-10-25 19:31:04 -0500

    tranformations csss
    tranformations css

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,7 @@ var people = fs.readFileSync("./data/people.json","utf8");
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'jesus christ',get:req.query,people:people});
 });
-
+router.get("/tranformaciones",(req,res)=>{
+  res.render("tranf",{title:"tranformaciones css"});
+});
 module.exports = router;
